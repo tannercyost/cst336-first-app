9c237e411945a111d46e99cd6820e2216fd76937
Tanner Yost <tyost@csumb.edu>
2018-12-19 23:49:29 +0000

    made progress on final exam

---
+++ cst336_final_exam/js/race.js
@@ -0,0 +1,23 @@
+function createModal() {
+    // Get the modal
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("openButton");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        $("#modalBox").css("display", "none");
+    }
+
+    var modal = document.getElementById('modalBox');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            $("#modalBox").css("display", "none");
+        }
+    }
+}
